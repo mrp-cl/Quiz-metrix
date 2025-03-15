11909e6bcc88a60fa15c55e6c1d70c3aec32a8fa
mjdsp <despi091402@gmail.com>
2025-03-15 15:15:58 +0800

    add config

---
+++ database/config.php
@@ -0,0 +1,29 @@
+<?php   
+    class Database{ 
+
+        private $hostName = "localhost";
+        private $userName ="root";
+        private $passWord = "";
+        private $database = "quizmetrix_db"; 
+
+        protected $conn; 
+
+        public function __construct()
+        {   
+            $this->conn = new mysqli($this->hostName,  
+                                      $this->userName,  
+                                      $this->passWord ,
+                                      $this->database); 
+            
+            if($this->conn->connect_errno) 
+            {
+                die("Connection failed: ". $this->conn->connect_errno);
+            }                         
+        } 
+
+        public function GetConnection()
+        {   
+            return $this->conn;
+        }  
+    } 
+?>
